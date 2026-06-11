4d0a1219bff3bb6da34dccb23f8315381e13b63a
ibuildrun <demiz1337@gmail.com>
2026-06-12 00:13:21 +0500

    deploy: font subset + inline FA + html minify

---
+++ _next/static/chunks/app/not-found-29bc45a8dcf6c246.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,931,191,3,626,495],{128:function(n,e,u){Promise.resolve().then(u.t.bind(u,4080,23))}},function(n){n.O(0,[223,971,23,744],function(){return n(n.s=128)}),_N_E=n.O()}]);
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/trading-course-landing',
-  trailingSlash: true,
-  images: { unoptimized: true },
-};
-export default nextConfig;
